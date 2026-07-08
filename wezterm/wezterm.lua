ff2de88cd50f7788339146939edc352183b78e1f
Bryson Bailey <brysonbailey94@gmail.com>
2026-07-07 19:30:17 -0500

    Maximize wezterm window on startup

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,4 +11,9 @@ config.window_background_opacity = 0.8
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
+
 return config
